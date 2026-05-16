883596988d4d7615a008fb4afcf429feca45f00e
Damilare-coder <oyinloyedamilare12@gmail.com>
2026-05-16 16:04:33 +0100

    This is my introduction file

---
+++ intro.js
@@ -0,0 +1,10 @@
+function me (name, age, prog) {
+ 
+
+    return me;
+}
+    let name = "Damilare";
+    let age = 22;
+    let prog = "JavaScript"
+
+console.log(`My name is ` + `${name}, I'm ${age}years old and my favorite programming language is ${prog}.`);
