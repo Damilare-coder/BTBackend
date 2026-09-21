f9bcdd3280ac518597628570326aeb13945e98e1
Damilare-coder <oyinloyedamilare12@gmail.com>
2026-09-21 19:08:42 +0100

    Today, I learnt and worked around how to create, read and update files in the present working directory in NodeJS, using fs.writeFileSync(), fs.readFileSync() and fs.appendFileSync().

---
+++ fileSystem-demo.js
@@ -0,0 +1,28 @@
+import { log } from "console";
+import fs from "fs";
+
+// fs.writeFileSync("message.txt","First message")
+
+// fs.writeFileSync("message.txt","Second message")
+
+//writeFileSync() replaces the existing content. It doesn't append to it.
+
+// const data = fs.readFileSync("message.txt","utf8")
+
+
+//Updating the file
+//appendFileSync() adds to the file
+// console.log(data)
+
+// fs.writeFileSync("message.txt", "First message\n");
+
+// fs.appendFileSync("message.txt", "Second message\n");
+
+// fs.appendFileSync("message.txt", "Third message\n");
+
+// const dataa = fs.readFileSync("message.txt", "utf8");
+
+// console.log(dataa);
+
+
+
---
+++ fs-exercise.js
@@ -0,0 +1,12 @@
+import fs from "fs"
+
+fs.writeFileSync("user.txt","Name:Damilare-coder\n")
+fs.appendFileSync("user.txt","Role:Backend Developer\n")
+
+
+// console.log(readUser);
+fs.appendFileSync("user.txt", "Learning NodeJS\n")
+
+const readUser = fs.readFileSync("user.txt", "utf8")
+
+console.log(readUser);
